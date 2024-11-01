4979698df1d7c45b32874880474ae0e6a1d176ed
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2024-11-01 11:35:54 -0300

    Criando adapter e centralizando as rotas

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,30 +1,14 @@
 import express from 'express';
 import { makerSignInController } from '../factories/makerSignInController';
 import { makerSignUpController } from '../factories/makerSignUpController';
+import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
 
 app.use(express.json())
 
-app.post('/sign-up', async (req, res) => {
-  const signUpController = makerSignUpController()
-
-  const {statusCode, body} = await signUpController.handle({
-    body: req.body
-  })
-
-  res.status(statusCode).json(body)
-});
-
-app.post('/sign-in', async (req, res) => {
-  const signInController = makerSignInController()
-
-  const {statusCode, body} = await signInController.handle({
-    body: req.body
-  })
-
-  res.status(statusCode).json(body)
-});
+app.post('/sign-up', routeAdapter(makerSignUpController()));
+app.post('/sign-in', routeAdapter(makerSignInController()));
 
 
 app.listen(3001, () => {
